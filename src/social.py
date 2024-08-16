dc0d752fea8841b40fa33c0fb9cd4a938776da01
Lumina <luminadang@gmail.com>
2024-08-16 16:35:13 -0700

    adding more example code to work from

--- src/social.py
+++ src/social.py
@@ -15,3 +15,41 @@ class Social:
         for rule in self.rules:
             if not rule.matches(context):
                 print("Rule violated:", rule)
+
+
+# match a literal first name and applying a regex to the email
+rule = rule_engine.Rule(
+    'first_name == "Luke" and email =~ ".*@rebels.org$"'
+) # => <Rule text='first_name == "Luke" and email =~ ".*@rebels.org$"' >
+rule.matches({
+    'first_name': 'Luke', 'last_name': 'Skywalker', 'email': 'luke@rebels.org'
+}) # => True
+rule.matches({
+   'first_name': 'Darth', 'last_name': 'Vader', 'email': 'dvader@empire.net'
+}) # => False
+
+
+
+characters = [
+  {
+    'name': 'BucketKnight',
+    'Class': 'Defender',
+  },
+  {
+    'name': 'SheepGirl',
+    'Class': 'Healer',
+  },
+]
+
+context = rule_engine.Context(type_resolver=rule_engine.type_resolver_from_dict({
+    'first_name': rule_engine.DataType.STRING,
+    'age': rule_engine.DataType.FLOAT
+}))
+
+# receive an error when an unknown symbol is used
+rule = rule_engine.Rule('last_name == "Vader"', context=context)
+# => SymbolResolutionError: last_name
+
+# receive an error when an invalid operation is used
+rule = rule_engine.Rule('first_name + 1', context=context)
+# => EvaluationError: data type mismatch
